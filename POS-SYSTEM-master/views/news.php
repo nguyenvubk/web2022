7229f94f48252aeccbd445b8d7d0fa4db08d2b26
Cuong251 <114635515+Cuong251@users.noreply.github.com>
2022-12-09 09:41:59 +0700

    Update news.php

---
+++ POS-SYSTEM-master/views/news.php
@@ -0,0 +1,146 @@
+<section class="latest-news">
+        <div class="container">
+            <div class="latest-news-head">
+                <h4>
+                <span style="color:#637ed6">
+                Những lợi ích bất ngờ của Coffee 
+                </span>  
+                </h4>
+                <p>
+                Tác dụng có lợi của cà phê đối với cơ thể con người đã được nhiều nghiên cứu chứng minh, nhưng nói chung, cà phê là một loại đồ uống phức tạp với hàng ngàn chất khác nhau. 
+                Không khó để nhận ra những lợi ích mà cafe mang lại, nhiều lợi ích sức khỏe quan trọng đã được khoa học chứng minh của cafe
+                </p>
+            </div>
+            <div class="row snippet-row">
+                <div class="col-lg-4 col-md-6">
+                    <div class="snippets">
+                        <h5>
+                        <span style="color:#3094bf">
+                        Cung cấp năng lượng và cải thiện khả năng tư duy
+                        </span> 
+                        </h5>
+                        <p>
+                        Cafe có thể giúp mọi người cảm thấy bớt mệt mỏi và tăng mức năng lượng.
+                        Nhiều nghiên cứu với độ tin cậy cao đã được tiến hành cho thấy ngoài khả năng hỗ trợ tăng cường dẫn truyền xung thần kinh, cafe còn có thể cải thiện các chức năng khác của não bộ.
+                        </p>
+                        <span><i class="fas fa-chevron-right"></i></span>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-md-6">
+                    <div class="snippets">
+                        <h5>
+                        <h5>
+                        <span style="color:#3094bf">
+                        Chứa nhiều chất dinh dưỡng thiết yếu
+                        </span> 
+                        </h5>
+                        <p>
+                        Nhiều chất dinh dưỡng có trong hạt cafe vẫn giữ nguyên giá trị dinh dưỡng khi được pha chế. Trung bình, một tách cafe chứa:
+                        Vitamin B2, Vitamin B5, Mangan và Kali, Magie và vitamin B đều là những chất cần thiết cho cơ thể.
+                        </p>
+                        <span><i class="fas fa-chevron-right"></i></span>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-md-6">
+                    <div class="snippets">
+                        <h5>
+                        <span style="color:#3094bf">
+                        Giảm nguy cơ mắc các bệnh tim mạch và đột quỵ
+                        </span>   
+                        </h5>
+                        <p>
+                        Nhiều nghiên cứu đã chứng minh cafe không làm tăng nguy cơ mắc bệnh tim mạch, thậm chí còn làm giảm nguy cơ, đặc biệt là đột quỵ. 
+                        Những người có thói quen uống cafe có nguy cơ mắc đột quỵ thấp hơn đến 20%.
+                        </p>
+                        <span><i class="fas fa-chevron-right"></i></span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    
+    <!--Features Section Ended-->
+    <!--News-Feed Section started-->
+    <section class="news-feed">
+        <div class="container-fluid p-0">
+            <div class="news-feed-inner">
+                <div class="news-feed-head text-center">
+                    <h4>
+                    <span style="color:#d62d44">
+                    Tin tức mới nhất
+                        </span>  
+                    </h4>
+                    <p>
+                        Cập nhật những tin tức mới nhất về Cafe và The Coffee House
+                    </p>
+                </div>
+                <div class="news-feed-content">
+                    <div class="row">
+                        <div class="col-lg-6 feed-col">
+                            <img src="images/news1.jpg" alt="Mobile News" class="img-fluid">
+                        </div>
+                        <div class="col-lg-6 feed-col">
+                            <div class="news-feed-details">
+                                <h4>
+                                <span style="color:#34bf75">
+                                MỪNG MERRY CLOUDTEA DIỆN MẠO MỚI <br>
+                                NHÀ CHƠI TỚI BẾN TẶNG QUÀ TO <br><br>
+                        </span>       
+                                </h4>
+                                <p>
+                                Từ 27.11 đến 10.12.2022, mỗi em được thêm một chiếc Mochi kem dẻo thơm núng nính, nhìn phát là yêu liền. Hổng chỉ vậy, 2 ẻm còn đổi outfit sang áo xanh cực xinh.<br> <br>
+                                🎁 Đặc biệt, Nhà chơi lớn tặng bạn quà khủng:<br>
+                                ✨ Khi ghé Nhà và mang ly Merry CloudTea đổi màu mua Very Berry Mochi hoặc S’More Choco Marshmallow, bạn được TẶNG 1 MERRY CLOUDTEA DIỆN MẠO MỚI.<br>
+                                👉 Mau ghé Nhà, rước quà bạn nha!<br>
+                                </p>
+                            </div>
+                        </div>
+                        <div class="col-lg-6 feed-col">
+                            <div class="news-feed-details">
+                                <h4>
+                                <span style="color:#34bf75">
+                                <br>
+                                <br>
+                                CÓ ĐÔI SẮP... CHIA TAY <br><br>
+                        </span>  
+                                </h4>
+                                <p>
+                                Hết ngày 10.12 này, chàng Mochi kem Phúc Bồn Tử phải quay về vũ trụ bánh, để nàng Mochi ở lại. Trong khi đó, anh Mochi kem Chocolate cũng chia tay mấy bé Marshmallow yêu dấu. <br>
+                                👉 Tranh thủ ghé Nhà (trừ cửa hàng tại điểm du lịch Phú Quốc và Bà Đen) để kịp gặp cặp đôi nghen bạn ơi!<br>
+                            </p>
+                            </div>
+                        </div>
+                        <div class="col-lg-6 feed-col">
+                            <img src="images/news2.png" alt="Mobile News" class="img-fluid">
+                        </div>
+                        <div class="col-lg-6 feed-col">
+                            <img src="images/news3.png" alt="Mobile News" class="img-fluid">
+                            <br>
+                            <br>
+                        </div>
+                        <div class="col-lg-6 feed-col">
+                            <div class="news-feed-details">
+                                <h4>
+                                <span style="color:#34bf75">
+                                <br>
+                                <br>
+                                155 “XỨ SỞ THẦN TIÊN” CHỜ BẠN GHÉ CHƠI <br><br>
+                        </span>  
+                                
+                                </h4>
+                                <p>
+                                Bạn ơi biết gì chưa? 😜<br>
+                                155 cửa hàng đã được trang hoàng lung linh rồi đấy.<br>
+                                Bởi Giáng Sinh này, Nhà không chỉ gửi đến bạn BST “Merry CloudTea - Nụ Hôn Thần Tiên” mà còn ấp ủ gửi xíu không khí cổ tích vào từng cửa hàng.<br>
+                                Lẹ làng, mình diện đồ đẹp đến “xử sở thần tiên” check-in nha. Biết đâu gặp được ông già “Nô en” biến ước mơ của bạn thành sự thật thì sao nè. 😉<br>
+                                🎄Còn chờ gì nữa, rủ ngay hội bạn ghé Nhà nha! <br><br>
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+  
+
